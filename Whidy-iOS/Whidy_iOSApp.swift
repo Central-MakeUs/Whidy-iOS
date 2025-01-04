13150a162e8aac4ef06c9592f83756f160acfa7b
jinoo <jinoo@namuplanet.com>
2025-01-04 16:30:17 +0900

    Initial Commit

---
+++ Whidy-iOS/Whidy_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Whidy_iOSApp.swift
+//  Whidy-iOS
+//
+//  Created by Namuplanet on 1/4/25.
+//
+
+import SwiftUI
+
+@main
+struct Whidy_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
