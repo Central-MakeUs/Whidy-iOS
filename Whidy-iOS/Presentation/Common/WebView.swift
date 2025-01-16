f30e0889e9127847c0e0794039d77e3bc08bf7f5
jinoo <jinoo@namuplanet.com>
2025-01-16 17:46:53 +0900

    [Fix] DeepLink Error Handling 로직 변경

--- Whidy-iOS/Presentation/Common/WebView.swift
+++ Whidy-iOS/Presentation/Common/WebView.swift
@@ -48,22 +48,16 @@ struct WebView : UIViewRepresentable {
         }
         
         func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
-            if let url = navigationAction.request.url {
-                // 딥링크인지 확인
-                if url.isDeepLink { // 딥링크 URL 스킴
-                    UIApplication.shared.open(url) { success in
-                        if success {
-                            Logger.debug("딥링크 실행 성공: \(url.absoluteString)")
-                        } else {
-                            Logger.debug("딥링크 실행 실패")
-                        }
-                    }
-                    
-                    decisionHandler(.cancel) // Redirect 중단
-                    return
+            guard let url = navigationAction.request.url else { return decisionHandler(.cancel) }
+            
+            if url.isDeepLink { // 딥링크 URL 스킴
+                UIApplication.shared.open(url) { isSuccess in
+                    if isSuccess { Logger.debug("DeepLink: \(url.absoluteString)") }
                 }
+                decisionHandler(.cancel) // Redirect 중단
+            } else {
+                decisionHandler(.allow)
             }
-            decisionHandler(.allow) // Redirect 허용
         }
         
         //FIXME: - Error 타입에 따라 Action 정의 필요함
@@ -74,8 +68,21 @@ struct WebView : UIViewRepresentable {
         
         /// 호출 시점: 초기 로드 요청 단계에서 실패
         func webView(_ webView: WKWebView, didFailProvisionalNavigation navigation: WKNavigation!, withError error: Error) {
+            // 에러 URL 확인
+            guard let url = (error as NSError).userInfo[NSURLErrorFailingURLStringErrorKey] as? String else {
+                Logger.error("Provisional navigation error: \(error)")
+                parent.store.send(.dismiss)
+                return
+            }
+
+            // 딥링크 스킴 확인
+            if let failingURL = URL(string: url), failingURL.isDeepLink {
+                Logger.info("Redirect URL is a deep link, skipping error handling: \(failingURL.absoluteString)")
+                return
+            }
+            
+            // 일반적인 에러 로그 출력
             Logger.error("Provisional navigation error: \(error)")
-            self.parent.store.send(.dismiss)
         }
         
     }
