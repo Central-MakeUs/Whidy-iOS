550445059828632f3986c5475912c72a7526461f
jinoo <jinoo@namuplanet.com>
2025-02-03 00:47:08 +0900

    [Fix] Filter Tag 목록 수정 - #2

--- Whidy-iOS/Presentation/Main/StudyMap/Feature/StudyMap/StudyMapFeature+Enum.swift
+++ Whidy-iOS/Presentation/Main/StudyMap/Feature/StudyMap/StudyMapFeature+Enum.swift
@@ -8,19 +8,51 @@
 import Foundation
 
 enum MapFilterCase : CaseIterable, Hashable, Identifiable {
-    case free
-    case franchise
-    case study
-    case library
-    case personal
+    case FREE_STUDY_SPACE
+    case FRANCHISE_CAFE
+    case GENERAL_CAFE
+    case STUDY_CAFE
+//    case FREE_CLOTHES_RENTAL
+//    case FREE_PICTURE
     
     var caseTitle : String {
         switch self {
-        case .free: return "무료공간"
-        case .franchise: return "프랜차이즈 카페"
-        case .study: return "스터디 카페"
-        case .library: return "도서관"
-        case .personal: return "개인 카페"
+        case .FREE_STUDY_SPACE: return "무료 공부 공간"
+        case .FRANCHISE_CAFE: return "프랜차이즈 카페"
+        case .GENERAL_CAFE: return "개인 카페"
+        case .STUDY_CAFE: return "스터디 카페"
+//        case .FREE_CLOTHES_RENTAL: return "정장 대여"
+//        case .FREE_PICTURE: return "면접 사진"
+        }
+    }
+    
+    var placeType : String {
+        switch self {
+        case .FREE_STUDY_SPACE : return "FREE_STUDY_SPACE"
+        case .FRANCHISE_CAFE : return "FRANCHISE_CAFE"
+        case .GENERAL_CAFE : return "STUDY_CAFE"
+        case .STUDY_CAFE : return "STUDY_CAFE"
+//        case .FREE_CLOTHES_RENTAL: return "FREE_CLOTHES_RENTAL"
+//        case .FREE_PICTURE: return "FREE_PICTURE"
+        }
+    }
+    
+    init?(from string: String) {
+        switch string {
+        case "FREE_STUDY_SPACE":
+            self = .FREE_STUDY_SPACE
+        case "FRANCHISE_CAFE":
+            self = .FRANCHISE_CAFE
+        case "GENERAL_CAFE":
+            self = .GENERAL_CAFE
+        case "STUDY_CAFE":
+            self = .STUDY_CAFE
+//        case "FREE_CLOTHES_RENTAL":
+//            self = .FREE_CLOTHES_RENTAL
+//        case "FREE_PICTURE":
+//            self = .FREE_PICTURE
+        default:
+            return nil
         }
     }
     
